27b0d951824279fb5814341fa5f982ca47e0e8f1
JoyMaveke <Mavekejoy@gmail.com>
2024-06-30 20:30:59 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;}
+
+
+   
+const leastFavoriteCustomer = 'some initial value';
+function changeLeastFavoriteCustomer() {
+        try {
+          leastFavoriteCustomer = 'new value';
+        } catch (e) {
+          throw new Error('Assignment to constant variable.');
+        }
+      }   
